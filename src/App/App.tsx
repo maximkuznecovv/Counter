b84942a135be036e62e99f37a15cc644028e43f2
maximkuznecovv <maximkuznecovv@gmail.com>
2021-12-18 14:32:15 +0300

    Counter 2.0 localstorage

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,6 +3,7 @@ import {Counter_localstorage} from "../Counter_localstorage/Counter_localstorage
 import {UseEffect} from "../Counter_localstorage_useEffect/useEffect";
 import {Counter_1} from "../Counter_1/Counter_1";
 import "./App.css"
+import {Counter_2} from "../Counter_2_localstorage/Counter_2";
 
 function App() {
 
@@ -10,6 +11,10 @@ function App() {
         <div className='app'>
             <div>Counter 1:</div>
             <Counter_1/>
+            <hr/>
+            <hr/>
+            <div>Counter 2.0 localstorage:</div>
+            <Counter_2/>
             <hr/>
             <hr/>
             <div>Counter localstorage:</div>
